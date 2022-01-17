8a5ed2db0452b6eb716535ce71f04ff5e64eadb4
sencho <senchoparameswara@gmail.com>
2022-01-17 15:08:38 +0700

    added main

---
+++ main.py
@@ -0,0 +1,8 @@
+# sintax pertama yaitu sequen yaitu eksekusi secara berurutan, berikut contohnya
+# print adalah sintaks untuk mencetak hasil, formatnya print()
+# jika ingin mencetak huruf atau sintaks yang diinginkan pakai ' ' atau " " dengan catatan harus konsisten
+print('hello world')
+print('my first code')
+# jika ingin mencetka perhitungan, tanpa menggunakan ''
+print(3 + 5)
+print(8 * 12 - 4)
