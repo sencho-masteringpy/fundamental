296547441d54586d6c0249085b3deae2710bb6f6
sencho <senchoparameswara@gmail.com>
2022-01-29 09:34:58 +0700

    delete code

--- sequen.py
+++
@@ -1,8 +0,0 @@
-# sintax pertama yaitu sequen yaitu eksekusi secara berurutan, berikut contohnya
-# print adalah sintaks untuk mencetak hasil, formatnya print()
-# jika ingin mencetak text atau syntax yang diinginkan pakai ' ' atau " " dengan catatan harus konsisten
-print('hello world')
-print('my first code')
-# jika ingin mencetka perhitungan, tanpa menggunakan ''
-print(3 + 5)
-print(8 * 12 - 4)
