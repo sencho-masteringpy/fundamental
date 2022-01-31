1fc52d5981e04b36dcf425947f7cd4d3e0aad1ab
sencho <senchoparameswara@gmail.com>
2022-01-31 11:33:09 +0700

    modifed code

--- looping-for.py
+++ looping-for.py
@@ -1,8 +1,21 @@
 number_of_book = 10
+
+# manually
+print('mom told you to read all your books')
+print('read book 1')
+print('read book 2')
+print('read book 3')
+print('read book 4')
+print('read book 5')
+print('read book 6')
+print('read book 7')
+print('read book 8')
+print('read book 9')
+print('read book 10')
+
+# using for
 print('mom told you to read all your books')
 
-read_books = 0
-
 for i in range(number_of_book):
-    read_books = read_books + 1
-    print(f'read book {read_books}')
+    print(f'read book {i+1}')
+
